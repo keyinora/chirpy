a6ee2e3bc49e1b21b08443b250f4a461ce0fe1e4
keyinora <ddraven1989@gmail.com>
2025-03-14 20:06:13 -0400

    basic server + counting server usages and reseting it

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # chirpy
 This is for the Course at Boot.Dev
+# rebuild project
+go build -o out && ./out
+# to stop the server manually.
+sudo lsof -i :8080
+# then use
+kill <PID>
+
+# try these command next time
+go run .
+ctrl+c to stop the server (didn't work)
--- main.go
+++ main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
-	mux := http.NewServeMux()
-	server := http.Server{
-		Handler: mux,
-		Addr:    ":" + port,
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
 	}
 
-	log.Printf("Serving on port: %s\n", port)
-	log.Fatal(server.ListenAndServe())
+	appHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux := http.NewServeMux()
+	mux.Handle("/app/", appHandler)
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
